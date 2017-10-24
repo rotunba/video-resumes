336888b93e47798484db0d391873d91ba90152db
rotunb200 <rotunb200@192.168.1.100>
2017-10-24 10:40:06 -0400

    First Commit

---
+++ src/main/java/com/phixbook/apps/videoresumes/VideoResumesApplication.java
@@ -0,0 +1,12 @@
+package com.phixbook.apps.videoresumes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoResumesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoResumesApplication.class, args);
+	}
+}
